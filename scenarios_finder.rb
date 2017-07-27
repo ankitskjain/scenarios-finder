476a07a1d484201aa5ea267a8c79cd23de71f253
ankitskjain <ankjain1986@gmail.com>
2017-07-27 15:22:14 +0100

    Updated code as per hack day on 27th

--- scenarios_finder.rb
+++ scenarios_finder.rb
@@ -10,29 +10,37 @@ tag = ''
 
 scenario = total_scenarios = total_debug = total_automated = total_manual = total_todo = total_wip = 0
 main_hash = Hash['@manual' => 0 , '@todo' => 0, '@debug' => 0, '@wip' => 0 ]
+auto_hash = Hash['automated' => 0]
 files.each do |file|
+  # Taking each feature file
   html_report.puts '<b>Checking the feature file : ' + file.split('cucumber/').last + '</b>'
+  # Reading first line of the feature file to check if we have given tags at the top
   first_line = File.open(file).first
+  # Initializing variables
   hash = Hash['@manual' => 0 , '@todo' => 0, '@debug' => 0, '@wip' => 0 ]
   first_line_tags = Array.new
   prev_line = ''
+  # Reading feature files
   File.open(file, 'r') do |f|
     scenario = 0
+    # Reading each line
     f.each_line do |line|
+      # Increase the Scenario count as soon as we found
       scenario += 1 if line.include? 'Scenario'
       hash.each do |tag, value|
+        # Checking for tag
         if first_line.include? tag
           first_line_tags.insert(0,tag)
           prev_line = first_line
           #first_line = first_line.gsub(tag, '')
         elsif line.include? tag
+          # Increasing the tag count as soon as found and its dynamic as well
           hash[tag] += 1
         end
       end
     end
     total_scenarios += scenario
-    total_tag = 0
-    naya = 0
+    total_tag = actual_tag = not_automated = 0
     html_report.puts '<p> Total number of scenarios in this feature file are : ' + scenario.to_s + '</p>'
     if prev_line != ''
       html_report.puts '<p> Found ' + first_line_tags.first.to_s + ' tag at the top of the file </p>'
@@ -45,14 +53,22 @@ files.each do |file|
     end
 
     hash.each do |f_tag, f_value|
-      html_report.puts '<p>Which also include ' + f_value.to_s + ' ' + f_tag.to_s + ' scenario(s) </p>' if not hash.include? first_line_tags.first.to_s
+      html_report.puts '<p>But we found ' + f_value.to_s + ' ' + f_tag.to_s + ' scenario(s) </p>' if f_value.to_s != '0' and f_tag.to_s != first_line_tags.first.to_s and hash.include? first_line_tags.first.to_s
+      html_report.puts '<p>Which include ' + f_value.to_s + ' ' + f_tag.to_s + ' scenario(s) </p>' if not hash.include? first_line_tags.first.to_s and f_value.to_s != '0'
     end
     if total_tag  > scenario
-      html_report.puts '<p> <font size="3" color="red">More tags found than number of scenarios</font> </p>'
-      naya = total_tag - scenario
-      hash[first_line_tags.first.to_s] = (scenario - naya).to_s
+      html_report.puts '<p> <font size="3" color="red">That means more tags found than number of scenarios</font> </p>'
+      actual_tag = total_tag - scenario
+      hash[first_line_tags.first.to_s] = (scenario - actual_tag).to_s
       html_report.puts '<p> So in this feature file ' + hash[first_line_tags.first.to_s] + ' ' + first_line_tags.first.to_s + ' scenario(s) </p>'
     end
+    # puts hash # Uncomment if you need to debug the hash
+    # Finding automated scenatrios
+    hash.each do |_k , v|
+      not_automated += v.to_i
+    end
+    html_report.puts '<p> Total number of automated scenario(s) in this feature file are : ' + (scenario - not_automated).to_s + '</p>'
+    puts total_automated += (scenario - not_automated).to_i
   end
   main_hash.each do |final_tag, final_count|
     main_hash[final_tag] += hash[final_tag].to_i
@@ -63,6 +79,10 @@ end
 html_report.puts '<h2>##### Final Report for ' + path.chomp + ' Repo ##### </h2>'
 html_report.puts '<p> Total number of feature files are ' + files.length.to_s + '</p>'
 html_report.puts '<p> Total Scenarios are ' + total_scenarios.to_s + '</p>'
+html_report.puts '<p> Total Automated Scenarios are ' + total_automated.to_s + '</p>'
+
+# puts main_hash # Uncomment if you need to debug the hash
+
 main_hash.each do |final_tags, final_counts|
   html_report.puts '<p> Number of ' + final_tags + ' tags ' + final_counts.to_s + '</p>'
 end
